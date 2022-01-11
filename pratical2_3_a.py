c8d5c91fe474c6f375372dfa95a256c2717f9fb4
20CE043 <97497245+20CE043@users.noreply.github.com>
2022-01-11 11:14:43 +0530

    Add files via upload

---
+++ pratical2_3_a.py
@@ -0,0 +1,15 @@
+#Name: Hena Kharwa
+#ID: 20CE043
+#Aim: Write a Python program to add member(s) in a set and clear a set.
+#Github link: https://github.com/20CE043/Practical-2-3.a.git 
+c=set()
+print(c)
+print("element add")
+#adding elements to the set
+c.add(1)
+c.add(3)
+c.add(5)
+print(c)
+#removing all elements from the set
+c.clear()
+print(c)
